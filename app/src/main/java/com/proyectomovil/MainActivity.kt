27cbfa3657b60c1041dd88713efe32654fe0f64d
Dantesakt1 <dvntecastillo@gmail.com>
2025-10-18 13:52:03 -0300

    Se agrega la validación a la conexión

--- app/src/main/java/com/proyectomovil/MainActivity.kt
+++ app/src/main/java/com/proyectomovil/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.proyectomovil
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.proyectomovil.conexionWAN.ValidarConexionWAN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //funcion heredada para verificar conexion a internet
+        if(ValidarConexionWAN.isOnline(this)){
+            println("conectado")
+        }else{
+            println("sin conexion")
+            val toast = Toast.makeText(
+                this
+                , "SIN CONEXION"
+                , Toast.LENGTH_SHORT).show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
